9e4668ac79ec590546327fa69d8ffb0f7aef1119
LarsBrebeckSWF <151862806+LarsBrebeckSWF@users.noreply.github.com>
2025-06-28 08:13:46 +0200

    Update 03_sprechpausen_erkennen.py

--- AD_DataPipeline_v01/03_sprechpausen_erkennen.py
+++ AD_DataPipeline_v01/03_sprechpausen_erkennen.py
@@ -1,1 +1,20 @@
-# Platzhalter für 03_sprechpausen_erkennen.py
+#Bibliotheken importieren
+import librosa
+
+#Lösungs-Code
+file_path = '/file.wav'
+y, sr = librosa.load(file_path, sr=22050)
+
+non_silent_intervals = librosa.effects.split(y, top_db=20)
+
+non_silent_intervals_in_seconds = [(start / sr, end / sr) for start, end in non_silent_intervals]
+print("Non silent intervals (in seconds):", non_silent_intervals_in_seconds)
+
+silent_intervals = []
+previous_end = 0
+for start, end in non_silent_intervals:
+    if start > previous_end:
+        silent_intervals.append((previous_end / sr, start / sr))
+    previous_end = end
+
+print("Silent intervals (in seconds):", silent_intervals)
