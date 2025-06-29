68b32cdbeb5b97cbb70f3a1800dbf25ef7f05130
Sven <sd_spam@outlook.de>
2025-06-29 23:47:59 +0200

    Szenenerkennung inkl. Screenshot extraktion.

--- AD_DataPipeline_v01/04_szenenerkennung.py
+++ AD_DataPipeline_v01/04_szenenerkennung.py
@@ -1,1 +1,12 @@
-# Platzhalter für 04_szenenerkennung.py
+from scenedetect import detect, ContentDetector, save_images
+
+def detect_scenes (file_path, screenshots = None): 
+    
+    scenes = detect(file_path, ContentDetector())
+    
+    if screenshots != None: 
+        images = save_images(scenes)
+        return (scenes, images)
+    else:
+        return scenes
+    
