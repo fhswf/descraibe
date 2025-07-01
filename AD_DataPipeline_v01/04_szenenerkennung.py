9c3adfc8801045df06b3d039bd0e1142dc6175f2
Sven <sd_spam@outlook.de>
2025-07-01 22:56:15 +0200

    04  Done

--- AD_DataPipeline_v01/04_szenenerkennung.py
+++ AD_DataPipeline_v01/04_szenenerkennung.py
@@ -1,12 +1,12 @@
-from scenedetect import detect, ContentDetector, save_images
+from scenedetect import detect, ContentDetector, save_images,open_video
 
-def detect_scenes (file_path, screenshots = None): 
-    
-    scenes = detect(file_path, ContentDetector())
-    
-    if screenshots != None: 
-        images = save_images(scenes)
-        return (scenes, images)
+
+#video_path = Pfad im Dateisystem, Screenshot Path = Pfad an den die Screenshots der dazugehörigen Timestamps gespeichert werden sollen
+def detect_scene_timestamps (video_path, screenshot_path = None): 
+    scene_timestamps = detect(video_path, ContentDetector())
+    if screenshot_path != None: 
+        videostream = open_video(video_path)
+        images = save_images(scene_list=scene_timestamps,video=videostream,output_dir=screenshot_path,num_images=1)
+        return (scene_timestamps, images)
     else:
-        return scenes
-    
+        return scene_timestamps
