8b6fdec1519c2ec0e20166c34ebae61bb57e0e63
Sven <sd_spam@outlook.de>
2025-07-13 02:15:13 +0200

    added comments for clarification

--- AD_DataPipeline_v01/04_szenenerkennung.py
+++ AD_DataPipeline_v01/04_szenenerkennung.py
@@ -1,9 +1,21 @@
 from scenedetect import detect, ContentDetector, save_images,open_video
 
+def detect_scene_timestamps (video_path:str, screenshot_path = None): 
 
-#video_path = Pfad im Dateisystem, Screenshot Path = Pfad an den die Screenshots der dazugehörigen Timestamps gespeichert werden sollen
-def detect_scene_timestamps (video_path, screenshot_path = None): 
+    """
+    Arguments:
+        video_path: Pfad des Videos im Dateisystem (str)
+        Optional: 
+            Screenshot Path: Pfad an den die Screenshots der dazugehörigen Timestamps gespeichert werden sollen (str). Falls Screnshot_path = None, dann werden keine Screenshots zum Szenenwechsel erzeugt
+
+    Returns:
+        Timestamps der Szenenwechsel (list)
+        Optional: 
+            Standbilder zu Beginn der neuen Szene werden in <Screenshot_path> abgelegt
+    """
+
     scene_timestamps = detect(video_path, ContentDetector())
+    print(type(scene_timestamps))
     if screenshot_path != None: 
         videostream = open_video(video_path)
         images = save_images(scene_list=scene_timestamps,video=videostream,output_dir=screenshot_path,num_images=1)
