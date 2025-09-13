e26621843441b39da7970e3cd7e639d30460a1f4
Sven <sd_spam@outlook.de>
2025-09-13 10:28:42 +0200

    Added Scene-Manager logic for more flexibility in parameterization. Also added possibility to add more images per "scene transition"

--- AD_DataPipeline_v01/04_szenenerkennung.py
+++ AD_DataPipeline_v01/04_szenenerkennung.py
@@ -1,12 +1,16 @@
-from scenedetect import detect, ContentDetector, save_images,open_video
+from scenedetect import SceneManager, ContentDetector, ThresholdDetector, AdaptiveDetector, HistogramDetector, HashDetector, save_images, open_video
 
-def detect_scene_timestamps (video_path:str, screenshot_path = None): 
+
+def detect_scene_timestamps (video_path:str, screenshot_path = None, frameskip = 0, kernel = AdaptiveDetector(), num_images=1): 
 
     """
     Arguments:
         video_path: Pfad des Videos im Dateisystem (str)
         Optional: 
             Screenshot Path: Pfad an den die Screenshots der dazugehörigen Timestamps gespeichert werden sollen (str). Falls Screnshot_path = None, dann werden keine Screenshots zum Szenenwechsel erzeugt
+            num_images: Anzahl der Bilder, die pro Szenenwechsel erzeugt werden sollen (int). Default = 1
+            frameskip: Anzahl Frames, die übersprungen werden sollen (int). verarbeite jeden N+1 frames, wobei N frameskip ist. Verarbeite also nur 1/N+1 Prozent des Videos 
+            kernel: Verwendeter Kern für die Erkennung. Erlaubte Werte: Contentdetector(), ThresholdDetector(), AdaptiveDetector(), HistogramDetector(), HashDetector()
 
     Returns:
         Timestamps der Szenenwechsel (list)
@@ -15,11 +19,15 @@ def detect_scene_timestamps (video_path:str, screenshot_path = None):
     """
 
     try:
-        scene_timestamps = detect(video_path, ContentDetector())
+
+        videostream = open_video(video_path)
+        scene_manager = SceneManager()
+        scene_manager.add_detector(kernel)
+        scene_manager.detect_scenes(videostream,frame_skip=frameskip)
+        scene_timestamps = scene_manager.get_scene_list()
         print(type(scene_timestamps))
         if screenshot_path != None: 
-            videostream = open_video(video_path)
-            images = save_images(scene_list=scene_timestamps,video=videostream,output_dir=screenshot_path,num_images=1)
+            images = save_images(scene_list=scene_timestamps,video=videostream,output_dir=screenshot_path,num_images=num_images)
             return (scene_timestamps, images)
         else:
             return scene_timestamps
@@ -29,3 +37,5 @@ def detect_scene_timestamps (video_path:str, screenshot_path = None):
         print(f"there was an error trying to parse the data{error}")
     except SyntaxError as error:
         print(f"Error processing path: {error}")
+
+
