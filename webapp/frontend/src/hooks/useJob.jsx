f37629181f17d42f8da95ec81c5dd98bc982ed03
Christian Gawron <gawron.christian@fh-swf.de>
2026-03-13 07:55:01 +0000

    fix: Import `useMemo` in `useJob` hook.

--- webapp/frontend/src/hooks/useJob.jsx
+++ webapp/frontend/src/hooks/useJob.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
+import React, { createContext, useContext, useState, useEffect, useCallback, useMemo } from 'react';
 
 const JobContext = createContext();
 
